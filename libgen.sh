3a4a90f81a42adc655c1734f0b6ce873cedba068
seantywork <>
2024-08-01 17:22:02 +0900

    [ 2024-08-01 17:22:02 ]: Automated CI push

--- libgen.sh
+++ libgen.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-rm -r ../lib
+mkdir -p lib
 
-rm -r ../lib.tgz
+/bin/cp -Rf libfactory/base ./lib/
 
-mkdir -p ../lib
-
-/bin/cp -Rf libfactory/base ../lib/
-
-/bin/cp -Rf libfactory/bin  ../lib/
+/bin/cp -Rf libfactory/bin  ./lib/
